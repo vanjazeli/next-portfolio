f0ec4f2b6c5da69a69397efbded73a2e78287b4e
vanjazeli <vanjazeli@gmail.com>
2024-07-26 20:28:57 +0200

    Add desktop version of the Header component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
+import Header from './components/containers/Header';
+
 export default function Home() {
 	return (
-		<div className="container px-5 mx-auto">
-			<div className="lg:w-full lg:flex lg:gap-5 oultine-1 min-h-dvh">
-				<div className="lg:w-1/2 outline-1">Test</div>
-				<div className="lg:w-1/2 outline-1">Content</div>
+		<div className="bg-slate-900">
+			<div className="container px-6 mx-auto lg:px-40">
+				<div className="lg:w-full lg:flex lg:gap-5 oultine-1 min-h-dvh">
+					<div className="lg:w-1/2">
+						<Header />
+					</div>
+					{/* <div className="lg:w-1/2">test</div> */}
+				</div>
 			</div>
 		</div>
 	);
