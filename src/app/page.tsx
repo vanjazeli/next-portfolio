3da9822f225807239227efd3bff029ce345aa10e
vanjazeli <vanjazeli@gmail.com>
2024-07-26 19:21:36 +0200

    Add main container

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
-import Icon from './components/ui/Icon';
-
 export default function Home() {
-	return <Icon className="w-20" variant="location" />;
+	return (
+		<div className="container px-5 mx-auto">
+			<div className="lg:w-full lg:flex lg:gap-5 oultine-1 min-h-dvh">
+				<div className="lg:w-1/2 outline-1">Test</div>
+				<div className="lg:w-1/2 outline-1">Content</div>
+			</div>
+		</div>
+	);
 }
