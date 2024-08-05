00d43f5dbde7534e5da227d77ce99414b7ee3828
vanjazeli <vanjazeli@gmail.com>
2024-08-05 22:41:01 +0200

    Add an additional spacer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,6 +20,7 @@ export default function Home() {
 							<MultipleParagraphs content={clientsAndProjectsContent} />
 							<Spacer isSmall />
 							<CardLinks />
+							<Spacer />
 						</main>
 					</div>
 				</div>
