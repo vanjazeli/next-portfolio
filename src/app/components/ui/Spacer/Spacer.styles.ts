4aabfae5e87b0429b7ef5ac74e5c21196dad8f15
vanjazeli <vanjazeli@gmail.com>
2024-08-11 15:18:52 +0200

    Move Spacer component styles to a specific file

---
+++ src/app/components/ui/Spacer/Spacer.styles.ts
@@ -0,0 +1,4 @@
+export const styles = {
+	small: 'h-6 md:h-10 lg:h-16',
+	large: 'h-12 md:h-20 lg:h-32',
+};
--- src/app/components/ui/Spacer/Spacer.tsx
+++ src/app/components/ui/Spacer/Spacer.tsx
@@ -1,7 +1,9 @@
+import { styles } from './Spacer.styles';
+
 type SpacerProps = {
 	isSmall?: boolean;
 };
 
 export default function Spacer({ isSmall }: SpacerProps) {
-	return <div className={`${isSmall ? 'h-6 md:h-10 lg:h-16' : 'h-12 md:h-20 lg:h-32'}`}></div>;
+	return <div className={isSmall ? styles.small : styles.large}></div>;
 }
