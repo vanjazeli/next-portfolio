1d3fa49ee2cd6d5dc8663366b948999bc92de9b2
vanjazeli <vanjazeli@gmail.com>
2024-08-11 14:05:55 +0200

    Move MultipleParagraphs component styles to a specific file

---
+++ src/app/components/containers/MultipleParagraphs/MultipleParagraphs.styles.ts
@@ -0,0 +1,6 @@
+export const styles = {
+	heading: 'text-3xl font-bold tracking-tight mb-4',
+	paragraphsContainer: 'flex flex-col gap-3',
+	paragraph: 'text-slate-400',
+	link: 'mt-10 lg:mt-8',
+};
--- src/app/components/containers/MultipleParagraphs/MultipleParagraphs.tsx
+++ src/app/components/containers/MultipleParagraphs/MultipleParagraphs.tsx
@@ -1,5 +1,6 @@
 import type { MultipleParagraphsContentT } from './MultipleParagraphs.types';
 import ArrowLink from '../../ui/ArrowLink/ArrowLink';
+import { styles } from './MultipleParagraphs.styles';
 
 type MultipleParagraphsProps = {
 	content?: MultipleParagraphsContentT;
@@ -12,15 +13,15 @@ export default function MultipleParagraphs({ content }: MultipleParagraphsProps)
 
 	return (
 		<section>
-			{heading && <h2 className="text-3xl font-bold tracking-tight mb-4">{heading}</h2>}
+			{heading && <h2 className={styles.heading}>{heading}</h2>}
 			{paragraphs && (
-				<div className="flex flex-col gap-3">
+				<div className={styles.paragraphsContainer}>
 					{paragraphs.map(({ text }, index) => (
-						<p className="text-slate-400" dangerouslySetInnerHTML={{ __html: text }} key={index} />
+						<p className={styles.paragraph} dangerouslySetInnerHTML={{ __html: text }} key={index} />
 					))}
 				</div>
 			)}
-			{link && <ArrowLink className="mt-10 lg:mt-8" href={link.url} text={link.text} target={link.target} variant={link.variant} />}
+			{link && <ArrowLink className={styles.link} href={link.url} text={link.text} target={link.target} variant={link.variant} />}
 		</section>
 	);
 }
