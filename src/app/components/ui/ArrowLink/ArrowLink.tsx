877cef535837456a789387e9d4cafa62a93f7373
vanjazeli <vanjazeli@gmail.com>
2024-08-11 14:09:26 +0200

    Move ArrowLink component styles to a specific file

--- src/app/components/ui/ArrowLink/ArrowLink.tsx
+++ src/app/components/ui/ArrowLink/ArrowLink.tsx
@@ -3,6 +3,7 @@ import Icon from '../Icon/Icon';
 import classNames from 'classnames';
 import type { ArrowLinkVariantT } from './ArrowLink.types';
 import type { LinkT } from '@/app/types/Link.types';
+import { styles } from './ArrowLink.styles';
 
 type ArrowLinkProps = LinkT & {
 	variant?: ArrowLinkVariantT;
@@ -12,18 +13,18 @@ type ArrowLinkProps = LinkT & {
 export default function ArrowLink({ href, text, target, variant = 'regular', className }: ArrowLinkProps) {
 	if (variant === 'regular') {
 		return (
-			<Link className={classNames('group flex align-center gap-1 text-slate-200 group-hover:text-teal-300 hover:text-teal-300 transition-colors w-fit', className)} href={href} target={target}>
-				<span className="font-bold">{text}</span>
-				<Icon className="w-4 group-hover:rotate-45 transition-transform" variant="arrow-top-right" />
+			<Link className={classNames(styles.regularLink, className)} href={href} target={target}>
+				<span className={styles.text}>{text}</span>
+				<Icon className={styles.regularIcon} variant="arrow-top-right" />
 			</Link>
 		);
 	}
 	if (variant === 'underline') {
 		return (
-			<Link className={classNames(`group flex align-center gap-1 text-slate-200 w-fit relative`, className)} href={href} target={target}>
-				<span className="font-bold">{text}</span>
-				<Icon className="w-4 group-hover:translate-x-1 transition-transform" variant="arrow-right" />
-				<span className="absolute left-0 bottom-0 right-0 h-[2px] transition-all bg-slate-200 opacity-0 group-hover:bg-teal-300 group-hover:opacity-100"></span>
+			<Link className={classNames(styles.underlineLink, className)} href={href} target={target}>
+				<span className={styles.text}>{text}</span>
+				<Icon className={styles.underlineIcon} variant="arrow-right" />
+				<span className={styles.underlineLine}></span>
 			</Link>
 		);
 	}
