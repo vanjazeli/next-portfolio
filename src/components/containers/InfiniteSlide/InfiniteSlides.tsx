694b82cc6df23856eb0bcfb9e627290cdf298736
vanjazeli <vanjazeli@gmail.com>
2024-08-13 23:27:24 +0200

    Remove margin-bottom for the last slider

--- src/components/containers/InfiniteSlide/InfiniteSlides.tsx
+++ src/components/containers/InfiniteSlide/InfiniteSlides.tsx
@@ -13,7 +13,7 @@ export default function InfiniteSlides({ content }: InfiniteSlidesProps) {
 	return (
 		<section>
 			{sliders && (
-				<div className="-translate-x-4 md:translate-x-0 w-[calc(100%+2rem)] md:w-auto flex flex-col gap-2">
+				<div className="-translate-x-4 md:translate-x-0 w-[calc(100%+2rem)] md:w-auto flex flex-col gap-2 mb-4 last:mb-0">
 					{sliders.map((slider, sliderIndex) => (
 						<div className="pb-px flex overflow-hidden relative lg:max-w-md before:bg-gradient-to-r before:from-slate-900 before:to-transparent before:h-full before:w-8 before:absolute before:top-0 before:left-0 before:z-10 after:bg-gradient-to-l after:from-slate-900 after:to-transparent after:h-full after:w-8 after:absolute after:top-0 after:right-0 after:z-10" key={sliderIndex}>
 							<ul className="animate-negative-infinite pl-2 flex flex-nowrap gap-2">
