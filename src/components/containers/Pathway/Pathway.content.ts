2e8b7a3013a686f02c3832467972326b89c12a71
vanjazeli <vanjazeli@gmail.com>
2024-08-18 18:51:40 +0200

    Change content for the career section

--- src/components/containers/Pathway/Pathway.content.ts
+++ src/components/containers/Pathway/Pathway.content.ts
@@ -5,14 +5,14 @@ export const careerListContent: PathwayContentT = {
 		{
 			top: {
 				link: {
-					href: '/',
+					href: 'https://www.vegaitglobal.com/',
 					text: 'Front-end Engineer - Vega IT',
 					target: '_blank',
 				},
 				period: 'May 2022 - Present',
 			},
 			timeframe: 'Fulltime',
-			description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veniam nisi, officiis, suscipit, amet qui earum eos quae maxime architecto voluptatem tempore pariatur doloremque doloribus quisquam cumque adipisci optio quod vero recusandae unde saepe nam accusamus. Quis ex quo et voluptatum neque necessitatibus asperiores nulla officiis possimus distinctio molestiae, alias autem!',
+			description: 'I contributed to many different projects, working on tasks such as animations, creating modules from scratch, and debugging existing codebases, with a primary focus on React and CSS.',
 			additionalInfo: [
 				{
 					text: 'React',
@@ -34,87 +34,35 @@ export const careerListContent: PathwayContentT = {
 		{
 			top: {
 				link: {
-					href: '/',
-					text: 'Front-end Engineer - Vega IT',
+					href: 'https://www.vegaitglobal.com/',
+					text: 'Front-end Developer - Vega IT',
 					target: '_blank',
 				},
-				period: 'May 2022 - Present',
+				period: 'Feb 2022 - Apr 2022',
 			},
-			timeframe: 'Fulltime',
-			description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veniam nisi, officiis, suscipit, amet qui earum eos quae maxime architecto voluptatem tempore pariatur doloremque doloribus quisquam cumque adipisci optio quod vero recusandae unde saepe nam accusamus. Quis ex quo et voluptatum neque necessitatibus asperiores nulla officiis possimus distinctio molestiae, alias autem!',
+			timeframe: 'Internship',
+			description: 'Main focus was setting up project starters using gulp.js, and then utilizing the same foundation to turn designs into functional pages.',
 			additionalInfo: [
 				{
-					text: 'React',
+					text: 'Umbraco',
 				},
 				{
-					text: 'Next.js',
+					text: 'Wordpress',
+				},
+				{
+					text: 'HTML',
+				},
+				{
+					text: 'SCSS',
+				},
+				{
+					text: 'Pixel Perfect',
 				},
 				{
 					text: 'JavaScript',
 				},
 				{
-					text: 'TypeScript',
-				},
-				{
-					text: 'SCSS',
-				},
-			],
-		},
-		{
-			top: {
-				link: {
-					href: '/',
-					text: 'Front-end Engineer - Vega IT',
-					target: '_blank',
-				},
-				period: 'May 2022 - Present',
-			},
-			timeframe: 'Fulltime',
-			description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veniam nisi, officiis, suscipit, amet qui earum eos quae maxime architecto voluptatem tempore pariatur doloremque doloribus quisquam cumque adipisci optio quod vero recusandae unde saepe nam accusamus. Quis ex quo et voluptatum neque necessitatibus asperiores nulla officiis possimus distinctio molestiae, alias autem!',
-			additionalInfo: [
-				{
-					text: 'React',
-				},
-				{
-					text: 'Next.js',
-				},
-				{
-					text: 'JavaScript',
-				},
-				{
-					text: 'TypeScript',
-				},
-				{
-					text: 'SCSS',
-				},
-			],
-		},
-		{
-			top: {
-				link: {
-					href: '/',
-					text: 'Front-end Engineer - Vega IT',
-					target: '_blank',
-				},
-				period: 'May 2022 - Present',
-			},
-			timeframe: 'Fulltime',
-			description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veniam nisi, officiis, suscipit, amet qui earum eos quae maxime architecto voluptatem tempore pariatur doloremque doloribus quisquam cumque adipisci optio quod vero recusandae unde saepe nam accusamus. Quis ex quo et voluptatum neque necessitatibus asperiores nulla officiis possimus distinctio molestiae, alias autem!',
-			additionalInfo: [
-				{
-					text: 'React',
-				},
-				{
-					text: 'Next.js',
-				},
-				{
-					text: 'JavaScript',
-				},
-				{
-					text: 'TypeScript',
-				},
-				{
-					text: 'SCSS',
+					text: 'gulp.js',
 				},
 			],
 		},
