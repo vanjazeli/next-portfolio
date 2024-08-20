cdf20fb987b6bdf942890dd09ed1edcd7a85ff3b
vanjazeli <vanjazeli@gmail.com>
2024-08-20 19:23:08 +0200

    Move the LoadingOverlay styles into a separate file

---
+++ src/components/client/LoadingOverlay/LoadingOverlay.styles.ts
@@ -0,0 +1,3 @@
+export const styles = {
+	overlay: 'fixed inset-0 bg-slate-900 z-50 transition-all duration-500',
+};
--- src/components/client/LoadingOverlay/LoadingOverlay.tsx
+++ src/components/client/LoadingOverlay/LoadingOverlay.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { useEffect, useRef, useState } from 'react';
+import { styles } from './LoadingOverlay.styles';
 
 export default function LoadingOverlay() {
 	const [isOpen, setIsOpen] = useState(true);
@@ -14,9 +15,8 @@ export default function LoadingOverlay() {
 	useEffect(() => {
 		if (overlay.current) {
 			overlay.current.style.opacity = '0';
-			overlay.current.style.backdropFilter = 'blur(0px)';
 		}
 	}, []);
 
-	return isOpen && <div className="fixed inset-0 bg-slate-900 z-50 backdrop-blur-3xl transition-all duration-500" ref={overlay} onTransitionEnd={hideOverlay}></div>;
+	return isOpen && <div className={styles.overlay} ref={overlay} onTransitionEnd={hideOverlay}></div>;
 }
