7d0534b33615cb9d084a38b1711830b246c9c796
vanjazeli <vanjazeli@gmail.com>
2024-08-27 09:08:32 +0200

    Change the Footer content

--- src/components/containers/Footer/Footer.content.ts
+++ src/components/containers/Footer/Footer.content.ts
@@ -6,7 +6,7 @@ export const footerContent: FooterContentT = {
 			text: '<h3 class="text-lg md:text-xl mb-1 last:mb-0 text-slate-200 font-bold">Thanks for stopping by! 👋</h3>',
 		},
 		{
-			text: '<p class="text-slate-400">Coded with love using Visual Studio Code, built with Next.js and Tailwind CSS, and deployed with GitHub. All text is set in the Inter typeface.</p>',
+			text: '<p class="text-slate-400">Coded with love using Visual Studio Code, built with Next.js and Tailwind CSS, and deployed with Vercel. All text is set in the Inter typeface.</p>',
 		},
 		{
 			text: '<p class="text-slate-400">Whether you\'re looking for inspiration, want to have a chat, or anything in between, please feel free to contact this awesome fella!</p>',
